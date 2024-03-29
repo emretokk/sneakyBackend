d64237aebcbce04247ac77ba36cf4452a5478133
Emre Tok <emretok35@gmail.com>
2024-03-29 23:01:29 +0300

    cors fix

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const sneakyRoutes = require("./src/products/routes");
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
-
+app.use(cors()); // Tüm kaynaklara erişim izni verir.
 app.use("/api/sneaky", sneakyRoutes);
 
 app.listen(port, () => console.log(`app listening on port: ${port}`));
